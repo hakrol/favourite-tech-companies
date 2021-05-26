7a2dae0bc8b0dcdf569a7f19a7d7b488a0542f74
Håkon Rolfsen <hakonrrolfsen@gmail.com>
2021-05-26 12:12:32 +0200

    Added strapi folder

---
+++ strapi-app/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '75775f47eb75f24aa535cf8192f49c46'),
+    },
+  },
+});
---
+++ strapi-app/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '9fca9c9c-ed4e-44f8-b2c8-13d2c1d12344'
+};
