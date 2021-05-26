443a90cb61b0dca3d2e68e722dee3fa8669a5b48
Håkon Rolfsen <hakonrrolfsen@gmail.com>
2021-05-26 13:55:08 +0200

    Cosmetic changes

--- js/components/showCompanies.js
+++ js/components/showCompanies.js
@@ -2,33 +2,30 @@ import { favButton } from "./favButton.js";
 import { getFavourites } from "./favFunctions.js";
 
 export function showCompanies(data, targetElement) {
-  const listContainer = document.querySelector(targetElement);
+    const listContainer = document.querySelector(targetElement);
 
-  const favourites = getFavourites();
+    const favourites = getFavourites();
 
-  listContainer.innerHTML = "";
+    listContainer.innerHTML = "";
 
-  data.forEach(function (company) {
-    let cssClass = "fa-circle";
+    data.forEach(function (company) {
+        let cssClass = "fa-circle";
 
-    const doesObjectExist = favourites.find(function (fav) {
-      return parseInt(fav.id) === company.id;
+        const doesObjectExist = favourites.find(function (fav) {
+            return parseInt(fav.id) === company.id;
+        });
+
+        if (doesObjectExist) {
+            cssClass = "fa-check-circle";
+        }
+
+        listContainer.innerHTML += `<li class="list-card">
+        <div class="list-card__content">
+          <h2>${company.name}</h2>
+        </div>
+        <i class="fa ${cssClass} fa-2x button" data-id="${company.id}" data-name="${company.name}" data-founded="${company.founded}" data-ceo="${company.ceo}" data-employees="${company.Employees}"></i>
+      </li>`;
     });
 
-    if (doesObjectExist) {
-      cssClass = "fa-check-circle";
-    }
-
-    listContainer.innerHTML += `<li class="list-card">
-        <div>
-          <h2>${company.name}</h2>
-          <p>Founded: ${company.founded}</p>
-          <p>CEO: ${company.ceo}</p>
-          <p>Employees: ${company.Employees}</p>
-        </div>
-        <i class="fa ${cssClass} fa-3x button" data-id="${company.id}" data-name="${company.name}" data-founded="${company.founded}" data-ceo="${company.ceo}" data-employees="${company.Employees}"></i>
-      </li>`;
-  });
-
-  favButton();
+    favButton();
 }
--- js/favourites.js
+++ js/favourites.js
@@ -7,12 +7,12 @@ let companiesContainer = document.querySelector(".list-wrapper ul");
 clearButton();
 
 if (favourites.length === 0) {
-  companiesContainer.innerHTML = `<h2>You need to choose some favourites!</h2>`;
+    companiesContainer.innerHTML = `<h2>You need to choose some favourites!</h2>`;
 }
 
 favourites.forEach((favourite) => {
-  companiesContainer.innerHTML += `<li class="list-card-fav">
-    <div>
+    companiesContainer.innerHTML += `<li class="list-card-fav">
+    <div class="list-card-fav__content">
       <h2>${favourite.name}</h2>
       <p>Founded: ${favourite.founded}</p>
       <p>CEO: ${favourite.ceo}</p>
